85ab3ef6299f8e21075b6ccce32d6093593cf7bf
unknown <ticketmalawi@gmail.com>
2025-05-23 18:20:49 +0200

    comm

--- src/home/linkBroker.jsx
+++ src/home/linkBroker.jsx
@@ -3,6 +3,7 @@ import styles from "./styles/linkBroker.module.css";
 
 const LinkBroker = ({ onClose }) => {
   const [selectedBroker, setSelectedBroker] = useState("");
+  const [selectedServer, setSelectedServer] = useState("");
   const [accountId, setAccountId] = useState("");
 
   const brokers = [
@@ -11,6 +12,14 @@ const LinkBroker = ({ onClose }) => {
     "Continental Capital",
     "Stockbrokers Malawi",
     "NBS Stockbrokers",
+  ];
+
+  const servers = [
+    "SERVER__001__NTR",
+    "SERVER__002__NTR",
+    "SERVER__003__NTR",
+    "SERVER__004__NTR",
+    "SERVER__005__NTR"
   ];
 
   return (
@@ -34,6 +43,18 @@ const LinkBroker = ({ onClose }) => {
             <option value="" disabled>Select a broker</option>
             {brokers.map((broker, index) => (
               <option key={index} value={broker}>{broker}</option>
+            ))}
+          </select>
+
+          <label htmlFor="server">Select Server</label>
+          <select
+            id="server"
+            value={selectedServer}
+            onChange={(e) => setSelectedServer(e.target.value)}
+          >
+            <option value="" disabled>Select a server</option>
+            {servers.map((server, index) => (
+              <option key={index} value={server}>{server}</option>
             ))}
           </select>
 
